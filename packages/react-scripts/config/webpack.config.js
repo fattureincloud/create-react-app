491446f1b37e3a8cdba69c2c7022a94889ae4809
Matteo Milesi <matteo.milesi@hotmail.it>
2019-10-29 22:43:30 +0100

    Modifiche per config babel.

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -434,6 +434,14 @@ module.exports = function(webpackEnv) {
                 // @remove-on-eject-end
                 plugins: [
                   [
+                    require.resolve('@babel/plugin-proposal-decorators'),
+                    { legacy: true },
+                  ],
+                  [
+                    require.resolve('@babel/plugin-proposal-class-properties'),
+                    { loose: true },
+                  ],
+                  [
                     require.resolve('babel-plugin-named-asset-import'),
                     {
                       loaderMap: {
