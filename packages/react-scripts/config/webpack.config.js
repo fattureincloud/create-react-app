50c51839db0d17f5a4fb3a9251f62a59d8a055af
Sebastian Senderowski <sebastiansenderowski@fattureincloud.it>
2021-05-05 17:41:08 +0200

    Implemented concat of REACT_APP_VERSION when creating dinamic url of chunks.js

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -231,7 +231,7 @@ module.exports = function (webpackEnv) {
         : isEnvDevelopment && 'static/js/[name].chunk.js',
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
-      publicPath: paths.cdnUrl,
+      publicPath: paths.cdnUrl + (process.env.REACT_APP_VERSION || ''),
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? (info) =>
